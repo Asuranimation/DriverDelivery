58812d491307fef286d678bd903e6709e2abf4df
Sakti Harahap <saktiharahap2606@gmail.com>
2023-09-10 08:41:35 +0700

    Update player script

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    
     [SerializeField] float speedRotate;
     [SerializeField] float speedMove;
-    [SerializeField] float slowSpeed;
+    [SerializeField] float slowSpeed = 2f;
     [SerializeField] float boostSpeed = 7f;
-   
+    [SerializeField] float boostDuration = 1f;
+    float normalSpeed = 4f;
+
+    private bool isBoosted = false;
+
     void Update()
     {
         RotateCapsule();
-        slowSpeed = 4f;
     }
 
     private void FixedUpdate()
@@ -42,5 +44,50 @@ public class Driver : MonoBehaviour
         }
 
         transform.Translate(new Vector3(0, move, 0));
+    }
+
+    private IEnumerator BoostSpeed()
+    {
+        isBoosted = true;
+        speedMove = boostSpeed;
+
+        yield return new WaitForSeconds(boostDuration);
+
+        // Setelah durasi boost selesai, kembalikan kecepatan ke nilai semula
+        speedMove = normalSpeed;
+        isBoosted = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            speedMove = slowSpeed;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            speedMove = normalSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Booster") && !isBoosted)
+        {
+            StartCoroutine(BoostSpeed());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Booster"))
+        {
+            speedMove = normalSpeed;
+
+        }
     }
 }
